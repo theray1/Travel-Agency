5ae7c790aa46cad66bd3716099814c8542895aa6
E194482T <EF2646cookie>
2022-03-09 11:50:21 +0100

    test add and delete step

--- src/test/java/fr/unantes/sce/calendar/TravelTest.java
+++ src/test/java/fr/unantes/sce/calendar/TravelTest.java
@@ -1,40 +1,57 @@
 package fr.unantes.sce.calendar;
 
+import fr.unantes.sce.people.Person;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class TravelTest {
+import static org.junit.jupiter.api.Assertions.*;
+
+class TravelTest {
+    private Travel travel;
+    private Calendar parent;
+    private Person owner;
+
     @BeforeEach
     public void setUp() throws Exception {
+        this.owner = new Person("Test", "admin");
+        this.parent = new Calendar(this.owner);
+        this.travel = new Travel(this.parent);
     }
 
     @Test
-    void getParent() {
+    void addCorrespondance() {
+        Correspondence step1 = new Correspondence(this.travel, new City("France", "Nantes"), new City("France", "Paris"), 12, 16);
+        Correspondence step2 = new Correspondence(this.travel, new City("France", "Paris"), new City("France", "Lyon"), 17, 20);
+        Correspondence step3 = new Correspondence(this.travel, new City("France", "Lyon"), new City("France", "Toulouse"), 8, 10);
+        Correspondence step4 = new Correspondence(this.travel, new City("France", "Toulouse"), new City("France", "Marseille"), 16, 18);
+        try {
+            this.travel.addCorrespondence(step1);
+            this.travel.addCorrespondence(step2);
+            this.travel.addCorrespondence(step3);
+            this.travel.addCorrespondence(step4);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
 
-    }
+        assertEquals(this.travel.getFirstStep(), step1);
+        assertEquals(this.travel.getLastStep(), step4);
 
-    @Test
-    void setParent() {
-    }
+        assertEquals(this.travel, step1.getTravel());
 
-    @Test
-    void getFirstStep() {
-    }
+        assertTrue(this.travel.removeCorrespondence(step4));
+        assertFalse(this.travel.removeCorrespondence(step4));
 
-    @Test
-    void getLastStep() {
-    }
+        assertEquals(this.travel.getLastStep(), step3);
 
-    @Test
-    void addCorrespondence() {
-    }
+        assertNull(step4.getTravel());
 
-    @Test
-    void removeCorrespondence() {
-    }
+        //Check 10 steps max
+        assertThrows(Exception.class, () -> {
+            for (int i = 0; i < 8; i++) {
+                this.travel.addCorrespondence(step1);
+            }
+        });
 
-    @AfterEach
-    public void tearDown() throws Exception {
     }
 }
